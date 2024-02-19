85a4433a81806edad66fea529d710987cb7f655b
HarshitChejara <harshitchejara29@gmail.com>
2024-02-19 14:30:45 +0530

    first commit

---
+++ links.js
@@ -0,0 +1,5 @@
+export const NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN =
+  "4ef3a96503fec8ee66d327703b4727f4";
+
+export const NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN =
+  "https://perfect-interior-design.myshopify.com";
